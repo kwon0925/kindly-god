001119c452439fe5127047d725a15a3a0ad79226
goblack0704 <goblack0704@gmail.com>
2026-03-16 18:10:24 +0900

    init kindly_god flutter webcd

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -44,7 +44,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "be23ccf245c590d3ca49b61de79bfba1
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "4e0f7a0ed88b63c4217a121045bf79a7",
+"flutter_bootstrap.js": "6da40a42715e91242c569b86563a1db2",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
